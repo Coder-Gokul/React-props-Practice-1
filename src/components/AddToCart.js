c7694f5708eb09758bf3fc718ea41afb08aaf8ee
Coder-Gokul <119941012+Coder-Gokul@users.noreply.github.com>
2024-03-03 14:47:45 +0530

    Added Alert

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,17 +1,20 @@
-import React from 'react'
-
+import React from "react";
 
 const AddToCart = (props) => {
-  const addedtocart = ()=>{
-    console.log(`The Book : "${props.title}" added to your cart`)
-  }
+  const addedtocart = () => {
+    console.log(`The Book : "${props.title}" added to your cart`);
+    alert(`The Book : "${props.title}" added to your cart`);
+  };
   return (
-    <div className='flex'>
-      <button className='bg-blue-700 text-white py-2 px-6 rounded-2xl hover:translate-y-[-2px] duration-200 shadow-lg hover:shadow-blue-500' onClick={addedtocart}>
+    <div className="flex">
+      <button
+        className="bg-blue-700 text-white py-2 px-6 rounded-2xl hover:translate-y-[-2px] duration-200 shadow-lg hover:shadow-blue-500"
+        onClick={addedtocart}
+      >
         Add to cart
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default AddToCart
+export default AddToCart;
--- src/components/BuyNow.js
+++ src/components/BuyNow.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const BuyNow = (props) => {
-    const buy = ()=>{
-        console.log(`The Price of "${props.title}" book is ${props.price}`)
-    }
+  const buy = () => {
+    console.log(`The Price of "${props.title}" book is ${props.price}`);
+    alert(`The Price of "${props.title}" book is ${props.price}`);
+  };
   return (
     <div>
-      <button className='bg-green-700 text-white py-2 px-8 rounded-2xl hover:translate-y-[-2px] duration-200 shadow-lg hover:shadow-green-500'onClick={buy}>
+      <button
+        className="bg-green-700 text-white py-2 px-8 rounded-2xl hover:translate-y-[-2px] duration-200 shadow-lg hover:shadow-green-500"
+        onClick={buy}
+      >
         Buy now
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default BuyNow
+export default BuyNow;
